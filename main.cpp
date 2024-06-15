9b20e1403593da3d1ff42a6ade2539817fcd231a
unknown <amnasiddiqui308@gmail.com>
2024-06-15 21:22:21 +0500

    Game can be played by 2 players now.

--- main.cpp
+++ main.cpp
@@ -19,13 +19,11 @@ void displayBoard(char board[3][3]) {
 	}
 }
 
-void terminalCases(char board[3][3], bool& gameDraw) {
-	bool checkWin = false;
-
+void terminalCases(char board[3][3], bool& checkWin) {
 	//Player Wins
 	for (int i = 0; i < 3; i++) {
 		for (int j = 1; j < 3; j++) {
-			if (board[i][j] == board[i][j - 1])
+			if ((board[i][j] == board[i][j - 1]) && board[i][j] != ' ')
 				checkWin = true;
 
 			else {
@@ -43,7 +41,7 @@ void terminalCases(char board[3][3], bool& gameDraw) {
 	if (!checkWin) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 1; j < 3; j++) {
-				if (board[j][i] == board[j - 1][i])
+				if ((board[j][i] == board[j - 1][i]) && board[j][i] != ' ')
 					checkWin = true;
 
 				else {
@@ -58,9 +56,9 @@ void terminalCases(char board[3][3], bool& gameDraw) {
 			}
 		}
 	}
-
+	
 	if (!checkWin) {
-		if (((board[0][0] || board[0][2]) == board[1][1]) && (board[1][1] == board[2][2])) {
+		if (((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[1][1] != ' ') || ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && board[1][1] != ' ')) {
 			checkWin = true;
 			cout << "Player " << board[1][1] << " won!" << endl;
 			return;
@@ -72,16 +70,18 @@ void terminalCases(char board[3][3], bool& gameDraw) {
 
 	//Game is Draw
 	if (!checkWin) {
+		checkWin = true;
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (board[i][j] == ' ') {
-					gameDraw = false;
+					checkWin = false;
 					break;
 				}
 			}
 		}
 
-		if (gameDraw) {
+		if (checkWin) {
 			cout << "Game is Draw :(" << endl;
 		}
 	}
@@ -90,21 +90,33 @@ void terminalCases(char board[3][3], bool& gameDraw) {
 void takingInput(char board[3][3], char player) {
 	int row = 0, col = 0;
 
+	cout << "Player " << player << "'s Turn!" << endl;
+
 	do {
 		cout << "Enter your move (row and column between 0 to 2)" << endl;
 		cin >> row >> col;
 
-		if (row < 0 || row > 2 || col < 0 || col > 2) {
+		if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
 			cout << "INVALID BOX HAS BEEN CHOSEN!\nEnter your move again." << endl;
 		}
-	} while (row < 0 || row > 2 || col < 0 || col > 2);
+
+	} while (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ');
 
 	board[row][col] = player;
 }
 
+// to change player after each turn
+char turn(char& player1, char& player2) {
+	char temp = player1;
+	player1 = player2;
+	player2 = temp;
+
+	return temp;
+}
+
 int main() {
 	char board[3][3], player1 = 'X', player2 = 'O';
-	bool gameDraw = true;
+	bool checkWin = false;
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -113,7 +125,13 @@ int main() {
 	}
 
 	while (1) {
+		
 		displayBoard(board);
-	}
+		terminalCases(board, checkWin);
 
+		if (checkWin)
+			exit(0);
+
+		takingInput(board, turn(player1, player2));
+	}
 }
